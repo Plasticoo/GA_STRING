abfc9497291a3c9eb2b41f38f706bdac41dcf8dd
Romeu Gomes <oh4ianon@hotmail.com>
2016-07-20 02:28:30 +0000

    Finally deleted the binary file and results file.

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-scan-build g++ -std=c++11 -Wall -pipe -march=native -O3 main.cpp -o program
+#scan-build g++ -std=c++11 -Wall -pipe -march=native -O3 main.cpp -o program
+scan-build clang++ -std=c++11 -Wall -pipe -march=native -O3 -ggdb main.cpp -o program
